57ef72f9671d3ba9bb15d802f1ced52574683320
davebecoding <d01001010@csu.fullerton.edu>
2022-06-05 18:30:40 -0700

    file READ/WRITE examples, 02

---
+++ src/Files/file_READ_02.cpp
@@ -0,0 +1,22 @@
+// reading a text file
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    std::string line;
+    std::ifstream myfile("example.txt");
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line))
+        {
+            std::cout << line << '\n';
+        }
+        myfile.close();
+    }
+
+    else
+        std::cout << "Unable to open file";
+
+    return 0;
+}
---
+++ src/Files/file_WRITE__02.cpp
@@ -0,0 +1,17 @@
+// writing on a text file
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    std::ofstream myfile("example.txt");
+    if (myfile.is_open())
+    {
+        myfile << "This is a line.\n";
+        myfile << "This is another line.\n";
+        myfile.close();
+    }
+    else
+        std::cout << "Unable to open file";
+    return 0;
+}
