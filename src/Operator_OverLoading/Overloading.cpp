dd4c9828ec5306c4da54d9a58cb84bb97845cd97
davebecoding <d01001010@csu.fullerton.edu>
2022-06-06 19:35:11 -0700

    Created time Threads for customer numbers

--- src/Operator_OverLoading/Overloading.cpp
+++ src/Operator_OverLoading/Overloading.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <stdlib.h>
+#include <chrono>
+#include <thread>
+#include <time.h>
 
 using namespace std;
 
@@ -8,29 +11,56 @@ struct Customers
     std::string Name;
     int CustomerItems;
 
-    Customers(string name, int customerItems)
-        : Name(name), CustomerItems(customerItems) {}
+    Customers(string name,
+              int customerItems)
+        : Name(name),
+          CustomerItems(customerItems) {}
 };
 
-ostream &operator<<(ostream &COUT, Customers &clientInfo);
+ostream &operator<<(ostream &COUT,
+                    Customers &clientInfo);
+int CustomerID();
 
-int main(int argc, char const *argv[])
+int main(int argc,
+         char const *argv[])
 {
-    int C_id = 0;
-    C_id = rand() % 30 + 1985;
 
-    Customers cust_01 = Customers("John ", C_id);
-    Customers cust_02 = Customers("Jane ", C_id);
+    using namespace std::this_thread; // sleep_for, sleep_until
+    using namespace std::chrono;      // nanoseconds, system_clock, seconds
 
-    cout << cust_01 << cust_02;
+    Customers cust_01 = Customers("John ",
+                                  CustomerID());
+    sleep_for(nanoseconds(10));
+    sleep_until(
+        system_clock::now() + seconds(1));
+
+    Customers cust_02 = Customers("Jane ",
+                                  CustomerID());
+    cout
+        << cust_01
+        << cust_02;
 
     return 0;
 }
 
-ostream &operator<<(ostream &COUT, Customers &clientInfo)
+int CustomerID()
 {
-    COUT << "Name : " << clientInfo.Name << endl;
-    COUT << "Customer Item # " << clientInfo.CustomerItems << endl;
+    int C_id;
+    srand(time(NULL));
+    C_id = rand() % 1000;
+    return C_id;
+}
+
+ostream &
+operator<<(ostream &COUT,
+           Customers &clientInfo)
+{
+    COUT << "Name : "
+         << clientInfo.Name
+         << endl;
+    COUT << "Customer Item # "
+         << clientInfo.CustomerItems
+         << endl;
 
     return COUT;
 }
