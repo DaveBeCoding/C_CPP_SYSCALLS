2da6524ae188bb177315a7f361d554a407713e89
davebecoding <d01001010@csu.fullerton.edu>
2022-06-09 22:40:09 -0700

    stage GET.cpp layout, with function headers, create/research blocks

--- src/Networking_C++/Get.cpp
+++ src/Networking_C++/Get.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <cpr/cpr.h>
+// gumbo (parser)
+
+/*
+1. Build function for GET request
+2. Pass GET response to parser function
+3. Pass parsed string(s) to file for python dataframe(s)
+*/
 
 using namespace std;
 
+std::string get_extract();
+void element_lookup(/*GumboNode *node*/);
+void links(/*GumboNode *node*/);
+
 int main(int argc, char **argv)
 {
+    /*
 
+
+    */
     auto url = cpr::Url{"https://en.wikipedia.org/wiki/Wikipedia"};
     // auto parameters = cpr::Parameters{{"hello", "world"}};
     cpr::Session session;
