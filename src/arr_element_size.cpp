f48c331448f5d3147ce9d5ae3decbaa4ed01dbda
davebecoding <d01001010@csu.fullerton.edu>
2022-06-14 17:48:46 -0700

    testing template, and getting type value for comparisons

--- src/arr_element_size.cpp
+++ src/arr_element_size.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <typeinfo>
 
-#define ARRAYSIZEA(x) (sizeof(x) / sizeof(x[0]))
+#define ARRAYSIZEA(x) (sizeof(x) / sizeof(x[0])) // Macro,
+// when used in library only use template version of function
 
 template <typename T, size_t N>
-size_t ARRAYSIZEB(T (&y)[N])
+size_t ARRAYSIZEB(T (&y)[N]) // make this a shared library
 {
     return N;
 }
@@ -12,8 +14,20 @@ int main(int argc, char const *argv[])
 {
     int arrayA[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     int arrayB[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    char arrayC[] = {'a', 'b', 'c', 'd', 'e'};
+    std::string arrayS[] = {"A", "B", "C", "D"};
 
-    std::cout << ARRAYSIZEA(arrayA) << std::endl;
-    std::cout << ARRAYSIZEB(arrayB) << std::endl;
+    std::cout << ARRAYSIZEA(arrayA)
+              << "\t" << typeid(arrayA).name()
+              << std::endl;
+    std::cout << ARRAYSIZEB(arrayB)
+              << "\t" << typeid(arrayA).name()
+              << std::endl;
+    std::cout << ARRAYSIZEB(arrayC)
+              << "\t" << typeid(arrayA).name()
+              << std::endl;
+    std::cout << ARRAYSIZEB(arrayS)
+              << "\t" << typeid(arrayA).name()
+              << std::endl;
     return 0;
 }
