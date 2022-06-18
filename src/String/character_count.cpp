b49e0c2d8b510936e4929cf4595901512144f9d7
davebecoding <d01001010@csu.fullerton.edu>
2022-06-18 09:07:33 -0700

    finding character count of string

--- src/String/character_count.cpp
+++ src/String/character_count.cpp
@@ -4,13 +4,13 @@ int main(int argc, char const *argv[])
 {
     char test[] = "example";
     auto x = 0;
-
     for (; test[x] != '\0'; x++)
-    {
-        /* code */
+    { /* code */
     }
 
-    std::cout << "Lenghth is " << x << std::endl;
-
+    std::cout
+        << "Lenghth is "
+        << x
+        << std::endl;
     return 0;
 }
