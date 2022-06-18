0e481b10b91d777f7a2507eb754a6343f1bdbcd3
davebecoding <d01001010@csu.fullerton.edu>
2022-06-18 10:03:00 -0700

    toggle string capitalization, skip speciaal characters -> 0-9 %^&*()...

---
+++ src/String/Toggle_Case.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+const char START_CHAR('a');
+const int CHAR_TOGGLE(32);
+const int BASE_START(65);
+const char END_CHAR('z');
+const int BASE_END(90);
+
+int main(int argc, char const *argv[])
+{
+    char test[] = "ExAmPLe123";
+    auto x = 0;
+    for (; test[x] != '\0'; x++)
+    {
+        if (test[x] >= BASE_START && test[x] <= BASE_END)
+        {
+            test[x] += CHAR_TOGGLE;
+        }
+        else if (test[x] >= START_CHAR && test[x] <= END_CHAR)
+        {
+            test[x] -= CHAR_TOGGLE;
+        }
+    }
+
+    std::cout
+        << "Flipped string "
+        << test
+        << std::endl;
+    return 0;
+}
