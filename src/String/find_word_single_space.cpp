72d04ff2e49ec89d4756cb842c6c1abaa4246394
davebecoding <d01001010@csu.fullerton.edu>
2022-06-18 17:04:48 -0700

    search a sentence with singel space as word counter

---
+++ src/String/find_word_single_space.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+const char START_CHAR('a');
+const int CHAR_TOGGLE(32);
+const int BASE_START(65);
+const char END_CHAR('z');
+const int BASE_END(90);
+
+int main(int argc, char const *argv[])
+{
+    char test[] = "today was a good day, Ice Cube";
+    auto word(1);
+    auto x = 0;
+    for (; test[x] != '\0'; x++)
+    {
+        if (test[x] == ' ')
+        {
+            word++;
+        }
+        // else if (test[x] >= START_CHAR && test[x] <= END_CHAR)
+        // {
+        //     return 0;
+        // }
+    }
+
+    std::cout
+        << "Number of words "
+        << word
+        << std::endl;
+    return 0;
+}
