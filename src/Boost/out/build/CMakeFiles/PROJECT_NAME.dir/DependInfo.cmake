c26a2a9c78ea0c3801b35a0ece54e5f961e6c5bc
dave johnson <dave@daves-MacBook-Pro.local>
2022-06-21 13:39:36 -0700

    setting up boost library

---
+++ src/Boost/out/build/CMakeFiles/PROJECT_NAME.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/dave/Documents/src/c++/C_CPP_SYSCALLS/src/Boost/test_Boost_include.cpp" "CMakeFiles/PROJECT_NAME.dir/test_Boost_include.cpp.o" "gcc" "CMakeFiles/PROJECT_NAME.dir/test_Boost_include.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
