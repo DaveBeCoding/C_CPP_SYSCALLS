ac0a692fac3966768629c33214b306d898ed23df
Dave <david00100johnson@gmail.com>
2022-06-23 01:06:20 -0700

    updating cmake on vector, adding cmake template, and testing python tool on c++ project -> g_updater.py

---
+++ sample_cmake.md
@@ -0,0 +1,5 @@
+cmake_minimum_required(VERSION x.xx)
+
+project(ProjectName)
+
+add_executable(${PROJECT_NAME} advancedProgram.cxx )
---
+++ src/Vector/build/CMakeFiles/VectorExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VectorExample.dir/Vect_01_Example.cpp.o"
+  "CMakeFiles/VectorExample.dir/Vect_01_Example.cpp.o.d"
+  "VectorExample"
+  "VectorExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VectorExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
