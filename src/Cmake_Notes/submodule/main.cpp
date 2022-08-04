127f169639a751ccf0d812ce439146624481e0b8
DaveBeCoding <70411835+DaveBeCoding@users.noreply.github.com>
2022-08-04 13:39:26 -0600

    Update main.cpp

--- src/Cmake_Notes/submodule/main.cpp
+++ src/Cmake_Notes/submodule/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
-int main()
+int main(int argc, char const *argv[])
 {
-  std::cout << "Test sub\n";
-  
- return 0; 
+    std::cout << "test submodule for git/github\n";
+
+    GLFWwindow *window;
+    int width, height;
+
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        exit(EXIT_FAILURE);
+    }
+
+    window = glfwCreateWindow(300, 300, "GOTCHA!", NULL, NULL);
+    if (!window)
+    {
+        fprintf(stderr, "Failed to open GLFW window\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    return 0;
 }
