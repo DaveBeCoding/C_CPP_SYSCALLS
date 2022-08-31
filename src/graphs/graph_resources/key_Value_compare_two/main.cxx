cff0eb3e43a352db78b6af8e5e64a981b1c67fd3
dave <djohn10@sandia.gov>
2022-08-31 14:55:56 -0600

    second compare method

---
+++ src/graphs/graph_resources/key_Value_compare_two/main.cxx
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "externals/std_math/test.hpp" //Part of Automation Project-Script <starterpack++> ... ignore = true;
+#include <map>
+#include <utility>
+ 
+typedef std::pair<std::string, std::string> pair;
+ 
+namespace std
+{
+    template<typename T1, typename T2>
+    struct less<std::pair<T1, T2>>
+    {
+        bool operator()(const std::pair<T1, T2> &l,
+                        const std::pair<T1, T2> &r) const
+        {
+            if (l.first == r.first) {
+                return l.second > r.second;
+            }
+ 
+            return l.first > r.first;
+        }
+    };
+}
+ 
+int main()
+{
+    std::map<pair, int> map =
+    {
+        { std::make_pair("C++", "C++14"), 2014 },
+        { std::make_pair("C++", "C++17"), 2017 },
+        { std::make_pair("Java", "Java 7"), 2011 },
+        { std::make_pair("Java", "Java 8"), 2014 },
+        { std::make_pair("C", "C11"), 2011 }
+    };
+ 
+    for (const auto &entry: map)
+    {
+        auto key_pair = entry.first;
+        std::cout << "{" << key_pair.first << "," << key_pair.second << "}, "
+                  << entry.second << std::endl;
+    }
+ 
+    return 0;
+}
