12f3d84e0c4238148cc8cd6beecc905d9f6d0fbe
davebecoding <david00100johnson@gmail.com>
2022-10-31 21:53:58 -0700

    staging example 01

--- src/algoithmic_thinking/chapter1/main.cxx
+++ src/algoithmic_thinking/chapter1/main.cxx
@@ -1,11 +1,31 @@
 #include <iostream>
 #include "externals/std_math/test.hpp" //Part of Automation Project-Script <starterpack++> ... ignore = true;
 
+
+
+void identify_identical(int values[], int n) {
+int i, j;
+for (i = 0; i < n; i++) {
+for (j = i+1; j < n; j++) {
+if (values[i] == values[j]) {
+printf("Twin integers found.\n");
+return;
+}
+}
+}
+printf("No two integers are alike.\n");
+}
+
+
 int main(int argc, char const *argv[])
 {
-    int foo = some_really_hard_math(10,5);
+    //int foo = some_really_hard_math(10,5);
 
-    std::cout << "Sampel project -> Plug & Play -> update 00ß" << std::endl;
-    std::cout << foo << "\n";
-    return 0;
+    //std::cout << "Sampel project -> Plug & Play -> update 00ß" << std::endl;
+    //std::cout << foo << "\n";
+
+     int a[5] = {1, 2, 3, 1, 5};
+     identify_identical(a, 5);	
+       
+	return 0;
 }
