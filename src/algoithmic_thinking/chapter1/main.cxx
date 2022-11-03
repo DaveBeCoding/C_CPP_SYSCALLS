1ed7bfbba176188ec130f09f3339bc94c9dbd5e4
davebecoding <david00100johnson@gmail.com>
2022-11-03 09:22:43 -0700

    hash example nonstl

--- src/algoithmic_thinking/chapter1/main.cxx
+++ src/algoithmic_thinking/chapter1/main.cxx
@@ -2,8 +2,30 @@
 #include "externals/std_math/test.hpp" //Part of Automation Project-Script <starterpack++> ... ignore = true;
 #define SIZE 100000
 
-int main(void)
+#define hashsize(n) ((unsigned long)1 << (n))
+#define hashmask(n) (hashsize(n) - 1)
+unsigned long oaat(char *key, unsigned long len,
+                   unsigned long bits)
 {
-    std::cout << "test for hash function" << std::endl;
+    unsigned long hash, i;
+    for (hash = 0, i = 0; i < len; i++)
+    {
+        hash += key[i];
+        hash += (hash << 10);
+        hash ^= (hash >> 6);
+    }
+    hash += (hash << 3);
+    hash ^= (hash >> 11);
+    hash += (hash << 15);
+    return hash & hashmask(bits);
+}
+
+int main(void)
+{ // sample call of oaat
+    long snowflake[] = {1, 2, 3, 4, 5, 6};
+    // 2^17 is the smallest power of 2 that is at least 100000
+    unsigned long code = oaat((char *)snowflake,
+                              sizeof(snowflake), 17);
+    printf("%u\n", code);
     return 0;
 }
