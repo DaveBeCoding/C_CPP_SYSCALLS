95b764c53931e6b0df59fd17df38fafc2842613e
davebecoding <david00100johnson@gmail.com>
2022-11-09 00:05:43 -0800

    C++ CPP update boost dir

---
+++ src/Boost/test_B/main.cxx
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "externals/std_math/test.hpp" //Part of Automation Project-Script <starterpack++> ... ignore = true;
+
+int main(int argc, char const *argv[])
+{
+    int foo = some_really_hard_math(10,5);
+
+    std::cout << "C++ is so COOL!" << std::endl;
+    std::cout << foo << "\n";
+    return 0;
+}
