1d26812197fca50ee15c4eefdbce923546aa4ea3
Dave <david00100johnson@gmail.com>
2023-05-12 01:15:17 -0700

    Pure Virtual

---
+++ src/virtualFunctions/pureVirtual_02/build/CMakeFiles/Pure_Virtual.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pure_Virtual.dir/main.cxx.o"
+  "CMakeFiles/Pure_Virtual.dir/main.cxx.o.d"
+  "Pure_Virtual"
+  "Pure_Virtual.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pure_Virtual.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
